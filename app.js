24ec81bdeb144e8bc73cb3f2a2ad4e946fc07f3a
khaganii <xaqaniabdullayev99@gmail.com>
2023-04-21 00:51:06 +0400

    some codes updated after ksenia's e-mail

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 
-document.getElementById("item1").addEventListener('click', myFunc);
-document.getElementById("item2").addEventListener('click', myFunc);
-document.getElementById("item3").addEventListener('click', myFunc);
-document.getElementById("item4").addEventListener('click', myFunc);
-document.getElementById("item5").addEventListener('click', myFunc);
-document.getElementById("item6").addEventListener('click', myFunc);
-
-
-function myFunc(){
-    let id = this.id;
-    for (let i = 1; i <= document.getElementsByName("item").length; i++) {
-        document.getElementById("item"+i).className = "menu-items";
-    }
-    document.getElementById(id).className = "menu-items active";
+let items = document.getElementById("menu-ul").getElementsByClassName("menu-items");
+let triangles = document.getElementById("menu-ul").getElementsByClassName("triangle-down");
+for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener("click", function() {
+            let currentItem = document.getElementsByClassName("active");
+            currentItem[0].className = currentItem[0].className.replace(" active", "");
+            this.className += " active";
+            let currentTriangle = document.getElementsByClassName("tr-active");
+            currentTriangle[0].className = currentTriangle[0].className.replace(" tr-active", "");
+            this.children[1].className += " tr-active";
+        });
 }
